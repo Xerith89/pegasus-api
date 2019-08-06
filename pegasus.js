d9d4b60cb0381d42b671319309ffe1e1c48107ef
Alex Coupe <Alex.Coupe@slis.local>
2019-08-06 14:01:11 +0100

    Starting the express server

--- pegasus.js
+++ pegasus.js
@@ -1,1 +1,16 @@
-const pegasus = required('express');
+//Bring in dependancies
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const pegasus = express();
+
+//Mongo will be used to store input and output
+
+//Declare middleware
+pegasus.use(express.json());
+pegasus.use(helmet());
+pegasus.use(cors());
+
+const port = process.env.port;
+
+module.exports = pegasus.listen(port, () => console.log(`Pegasus Server Started On Port ${port}`));
