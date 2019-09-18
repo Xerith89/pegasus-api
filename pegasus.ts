852a7937a807fc7acaef0d60267614dc0058fb63
Alex Coupe <Alex.Coupe@slis.local>
2019-09-18 16:59:29 +0100

    Update to TS syntax

--- pegasus.ts
+++ pegasus.ts
@@ -1,46 +1,51 @@
 import ServiceController from './src/services/ServiceController';
 import ExampleScheme from './src/schemes/ExampleScheme';
 import ExampleSchemeModel from './src/schemes/ExampleSchemeModel';
+import express = require('express');
+import helmet = require('helmet');
+import cors = require('cors');
+
+import dotenv = require('dotenv');
 
 
-namespace Pegasus {
-    //Bring in core dependencies 
-    const express = require('express');
-    const helmet = require('helmet');
-    const cors = require('cors');
-    const pegasus = express();
-    const dotenv = require('dotenv');
+//TODO - console input commands, logging to files and console
 
-    const serviceContainer = new ServiceController();
-    const serviceRouter = require('./src/services/ServiceRouter');
+//Bring in core dependencies 
+const pegasus = express();
+const serviceRouter = require('./src/services/ServiceRouter');
+const serviceContainer = new ServiceController();
 
-    //Config our local settings
-    dotenv.config();
 
-    //Database setup
-    const connection = require('./database');
-    connection.connect();  
-    connection.end();
+//Config our local settings
+dotenv.config();
 
-    //Bring in your models
-    const exampleSchemeModel = new ExampleSchemeModel();
+//Bring in your models
+const exampleSchemeModel = new ExampleSchemeModel();
 
-    //Bring in your schemes, bind the model, name them and decide if they are exposed to the endpoint
-    const exampleScheme = new ExampleScheme(exampleSchemeModel, 'examplescheme', true);
-  
-    //Register your schemes in the service container here
-    serviceContainer.registerServices([exampleScheme]);
+//Bring in your schemes, bind the model, name them and decide if they are exposed to the endpoint
+const exampleScheme = new ExampleScheme(exampleSchemeModel, 'examplescheme', true);
 
-    //Start all registered services
-    serviceContainer.startServices();
+//Register your schemes in the service container here
+serviceContainer.registerServices([exampleScheme]);
 
-    //Declare middleware
-    pegasus.use(express.json());
-    pegasus.use(helmet());
-    pegasus.use(cors());
-    pegasus.use('/api/:name',serviceRouter);
+//Start all registered services
+serviceContainer.startServices();
 
-    const port = process.env.DEFAULT_PORT;
+//Declare middleware
+pegasus.use(express.json());
+pegasus.use(helmet());
+pegasus.use(cors());
+pegasus.use('/api/:name',serviceRouter);
 
-    module.exports = pegasus.listen(port, () => console.log(`Pegasus Server Started On Port ${port}`));
-}
+const port = process.env.DEFAULT_PORT;
+
+const server = pegasus.listen(port);
+
+    //Stop all services on server close
+server.on('close', function() {
+    serviceContainer.stopServices();
+});
+
+server.close();
+
+module.exports = server;
